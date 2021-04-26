6d965088557aba4cf0f0b87a7886b76f2315d190
Luka Dragar <D20124481@mytudublin.ie>
2021-04-26 02:58:30 +0100

    Tutorial is detecting sample collected

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -3,31 +3,35 @@ using UnityEngine.UI;
 
 public class PlayerStats : PlayerModule
 {
-    public float samplesRequired = 5f;
-    public float samplesCollected = 0f;
-    public Text collectedText;
-    public Text requiredText;
+public float samplesRequired = 5f;
+public float samplesCollected = 0f;
+public Text collectedText;
+public Text requiredText;
 
-	protected override void Awake()
-	{
-		base.Awake();
+protected override void Awake()
+{
+        base.Awake();
         requiredText.text = "" + samplesRequired;
-	}
+}
 
-    public void CollectSample(float amount)
-    {
+public void CollectSample(float amount)
+{
         samplesCollected += amount;
-        if (samplesCollected >= samplesRequired)
+        if (samplesCollected >= samplesRequired/2)
         {
-            samplesCollected = 0f;
-            OnEnoughSamplesCollected();
+                samplesCollected = 0f;
+                OnEnoughSamplesCollected();
         }
         // update UI
         collectedText.text = "" + (int)samplesCollected;
-    }
+}
 
-    void OnEnoughSamplesCollected()
-    {
-
-    }
+void OnEnoughSamplesCollected()
+{
+        GameObject tutorialLeveL = GameObject.Find("TutorialLevel");
+        if (tutorialLeveL != null)
+        {
+                tutorialLeveL.GetComponent<TutorialLevel>().EnableDeeperText();
+        }
+}
 }
--- Assets/Scripts/TutorialLevel.cs
+++ Assets/Scripts/TutorialLevel.cs
@@ -11,12 +11,21 @@ public GameObject deeperText;
 void Start()
 {
         Player.tr.position = new Vector3(0.2f, 7.3f, 0f);
-
-        StartCoroutine(EnableMineralsText());
+        Player.instance.stats.samplesRequired = 1f;
+        Player.instance.stats.requiredText.text = "1";
+        StartCoroutine(EnableMineralsTextCoroutine());
 }
-IEnumerator EnableMineralsText()
+
+public void EnableDeeperText()
 {
-        yield return new WaitForSeconds(0);
+        Player.instance.stats.collectedText.text = "" + (int) Player.instance.stats.samplesCollected;
+        deeperText.SetActive(true);
+        mineralsText.SetActive(false);
+}
+
+IEnumerator EnableMineralsTextCoroutine()
+{
+        yield return new WaitForSeconds(1);
         mineralsText.SetActive(true);
 }
 }

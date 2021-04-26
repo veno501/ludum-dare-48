abc2e2913f5d7e9f05202442cb07f93af3dd1479
Luka Dragar <D20124481@mytudublin.ie>
2021-04-26 01:54:30 +0100

    Started tutorial level

---
+++ Assets/Scripts/TutorialLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialLevel : MonoBehaviour
+{
+
+public GameObject mineralsText;
+public GameObject deeperText;
+
+void Start()
+{
+        Player.tr.position = new Vector3(0.2f, 7.3f, 0f);
+
+        StartCoroutine(EnableMineralsText());
+}
+IEnumerator EnableMineralsText()
+{
+        yield return new WaitForSeconds(0);
+        mineralsText.SetActive(true);
+}
+}

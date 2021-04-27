deb86938c22f7a731f8b637ed19fda7f942b48f0
Luka Dragar <D20124481@mytudublin.ie>
2021-04-27 01:38:26 +0100

    Stats menu and depth level

---
+++ Assets/Scripts/DepthUpdate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthUpdate : MonoBehaviour
+{
+// Start is called before the first frame update
+void Start()
+{
+
+}
+
+// Update is called once per frame
+void Update()
+{
+        int depth = (int) ((((16 - (((int) Player.tr.position.y + 8))) / 16f)) * 100);
+        if (depth < 10)
+                GetComponent<UnityEngine.UI.Text>().text = Level.instance.currentLayer.depth + "0" + depth + " M";
+        else if (depth >= 100)
+                GetComponent<UnityEngine.UI.Text>().text = Level.instance.currentLayer.depth+1 + "00 M";
+        else
+                GetComponent<UnityEngine.UI.Text>().text = Level.instance.currentLayer.depth + "" + depth + " M";
+
+
+
+}
+}
